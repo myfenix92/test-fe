6d049ea7b5306e03bc2bf65a2fcf6f7740402007
Yana Kornejkova <bloom.hog@gmail.com>
2021-10-14 13:01:21 +0300

    add files

---
+++ script.js
@@ -0,0 +1,25 @@
+let id = document.querySelector('.input_id')
+let nameCol = document.querySelector('.input_name')
+
+async function createUser (user) {
+  console.log(user)
+  const rawResponse = await fetch('https://kurs-db.herokuapp.com/db', {
+    mode: 'no-cors',
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(user),
+  },
+  console.log('fetch'));
+//  const content = await rawResponse.json();
+// console.log(content)
+};
+
+document.querySelector('.send_btn').addEventListener('click', () => {
+  createUser({
+    id: id.value,
+    name: nameCol.value
+  })
+})
